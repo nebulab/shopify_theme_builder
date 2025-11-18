2b8337042885bb22a46b58fc70e986f6f4e472f8
Massimiliano Lattanzio <massimiliano.lattanzio@gmail.com>
2025-11-18 14:54:51 +0100

    Add basic file watching functionality
    Implements a watch mode that monitors components folder for changes
    and triggers selective rebuilds. Performs an initial build on startup,
    then watches for file modifications and rebuilds
    only the changed files within the components directory.

--- lib/shopify_theme_builder.rb
+++ lib/shopify_theme_builder.rb
@@ -1,11 +1,52 @@
 # frozen_string_literal: true
 
+require "fileutils"
 require "logger"
 require_relative "shopify_theme_builder/version"
 require_relative "shopify_theme_builder/watcher"
 require_relative "shopify_theme_builder/liquid_processor"
 require_relative "shopify_theme_builder/builder"
 
+# The main module for ShopifyThemeBuilder.
 module ShopifyThemeBuilder
-  # Your code goes here...
+  class << self
+    def watch(folders_to_watch: ["_components"])
+      create_folders(folders_to_watch)
+
+      initial_build(folders_to_watch)
+
+      watch_folders(folders_to_watch)
+    end
+
+    private
+
+    def create_folders(folders_to_watch)
+      puts "Creating necessary folders..."
+
+      FileUtils.mkdir_p(folders_to_watch)
+      FileUtils.mkdir_p("sections")
+      FileUtils.mkdir_p("blocks")
+      FileUtils.mkdir_p("snippets")
+    end
+
+    def initial_build(folders_to_watch)
+      puts "Doing an initial build..."
+
+      folders_to_watch.each do |folder|
+        Builder.new(files_to_process: Dir.glob("#{folder}/**/*.*")).build
+      end
+    end
+
+    def watch_folders(folders_to_watch)
+      puts "Watching for changes in '#{folders_to_watch.join(", ")}' folders..."
+
+      Watcher.new(folders_to_watch).watch do |changes|
+        changes.each_key do |filename|
+          relative_filename = filename.gsub("#{Dir.pwd}/", "")
+
+          Builder.new(files_to_process: [relative_filename]).build if relative_filename.start_with?(*folders_to_watch)
+        end
+      end
+    end
+  end
 end
--- spec/shopify_theme_builder_spec.rb
+++ spec/shopify_theme_builder_spec.rb
@@ -4,4 +4,93 @@ RSpec.describe ShopifyThemeBuilder do
   it "has a version number" do
     expect(ShopifyThemeBuilder::VERSION).not_to be_nil
   end
+
+  describe ".watch" do
+    before do
+      allow(FileUtils).to receive(:mkdir_p)
+      allow(ShopifyThemeBuilder::Builder).to receive(:new).and_return(double(build: nil))
+      allow(ShopifyThemeBuilder::Watcher).to receive(:new).and_return(double(watch: nil))
+      allow(Dir).to receive_messages(pwd: "/path/to/project", glob: ["_components/button/block.liquid"])
+    end
+
+    it "outputs creating folders message" do
+      expect { described_class.watch }.to output(/Creating necessary folders/).to_stdout
+    end
+
+    it "creates necessary folders" do
+      described_class.watch
+
+      expect(FileUtils).to have_received(:mkdir_p).exactly(4).times
+    end
+
+    it "outputs initial build message" do
+      expect { described_class.watch }.to output(/Doing an initial build/).to_stdout
+    end
+
+    it "performs an initial build" do
+      described_class.watch
+
+      expect(ShopifyThemeBuilder::Builder).to have_received(:new)
+        .with(files_to_process: ["_components/button/block.liquid"])
+    end
+
+    it "outputs watching message" do
+      expect { described_class.watch }.to output(/Watching for changes in '_components' folder/).to_stdout
+    end
+
+    it "starts watching for file changes" do
+      described_class.watch
+
+      expect(ShopifyThemeBuilder::Watcher).to have_received(:new).with(["_components"])
+    end
+
+    it "sets up a watch block to handle file changes" do
+      spy = double(watch: nil)
+      allow(ShopifyThemeBuilder::Watcher).to receive(:new).and_return(spy)
+
+      described_class.watch
+
+      expect(spy).to have_received(:watch)
+    end
+
+    context "when files change in components folder" do
+      before do
+        spy = double(watch: nil)
+        allow(ShopifyThemeBuilder::Watcher).to receive(:new).and_return(spy)
+        allow(spy).to receive(:watch) do |&block|
+          changes = {
+            "/path/to/project/_components/button/schema.json" => :created
+          }
+          block&.call(changes)
+        end
+      end
+
+      it "processes files when changes are detected in components folder" do
+        described_class.watch
+
+        expect(ShopifyThemeBuilder::Builder).to have_received(:new)
+          .with(files_to_process: ["_components/button/schema.json"])
+      end
+    end
+
+    context "when files outside components folder change" do
+      before do
+        spy = double(watch: nil)
+        allow(ShopifyThemeBuilder::Watcher).to receive(:new).and_return(spy)
+        allow(spy).to receive(:watch) do |&block|
+          changes = {
+            "/path/to/project/_non_components_folder/button/block.liquid" => :updated
+          }
+          block&.call(changes)
+        end
+      end
+
+      it "does not process files outside components folder" do
+        described_class.watch
+
+        expect(ShopifyThemeBuilder::Builder).not_to have_received(:new)
+          .with(files_to_process: ["_non_components_folder/button/block.liquid"])
+      end
+    end
+  end
 end
